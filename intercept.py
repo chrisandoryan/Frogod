48ae25ee49198f18ac9afb24ccf88765c6ddff71
Chrisando Ryan <chrisando.siahaan@gmail.com>
2019-06-21 16:56:32 +0700

    added labelman.py and model training

--- intercept.py
+++ intercept.py
@@ -2,14 +2,13 @@ import scapy.all as scapy
 from scapy_http import http
 import argparse
 import json
+import csv
 import engine
 
 count = 1
 
-
 def sniff_packet(interface):
     scapy.sniff(iface=interface, store=False, prn=process_packets)
-
 
 def process_packets(packet):
     global count
@@ -29,13 +28,28 @@ def process_packets(packet):
         # print content_type
         # print referer
         # print(packet.show())
+        # print(packet[http.HTTPRequest])
+        
         inbound = url if args.get else load
+
+        # logs = pelotot.parsing(inbound)
+        # pelotot.analyze(logs)
+        
         for s in engine.tokenize(inbound):
             print("Writing inbound payload [{}]".format(count))
             s['request_method'] = request_method.decode("utf-8")
             s['user_agent'] = user_agent.decode("utf-8")
-            with open('samples/{}.txt'.format('GET' if args.get else 'POST'), 'a') as f:
+            if (args.label_normal):
+                s['label'] = 'normal'
+            elif (args.label_threat):
+                s['label'] = 'threat'
+            with open('samples/{}.txt'.format('GET' if args.get else 'POST2'), 'a') as f:
                 f.write(json.dumps(s) + "\n")
+            with open('samples/{}.csv'.format('GET' if args.get else 'POST2'), 'a') as f:
+                # convert {0: 1.5, 1: 2.2, ...} to [1.5, 2.2, ...] to 1.5 2.2, ...
+                s['centrality'] = ' '.join(map(str, list(s['centrality'].values())))
+                writer = csv.writer(f)
+                writer.writerow(list(s.values()))
             count += 1
 
 
@@ -67,7 +81,9 @@ def get_payload(packet):
     if packet.haslayer(scapy.Raw):
         return packet[scapy.Raw].load
 
-parser = argparse.ArgumentParser(description='Packet interceptor for Cyber Security Reseach Team')
+parser = argparse.ArgumentParser(description='Packet inspection for Cyber Security Reseach Team')
+parser.add_argument('--label-threat', action='store_true', help='Auto-labelling inbound payload as a threat')
+parser.add_argument('--label-normal', action='store_true', help='Auto-labelling inbound payload as normal payload')
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument('--get', action='store_true', help='Capture GET request')
 group.add_argument('--post', action='store_true', help='Capture POST request')
---
+++ train.py
@@ -0,0 +1,57 @@
+from sklearn import svm
+import pandas as pd  
+import numpy as np  
+import matplotlib.pyplot as plt  
+
+from sklearn.model_selection import train_test_split  
+from sklearn.svm import SVC  
+from sklearn.metrics import classification_report, confusion_matrix  
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import accuracy_score
+from sklearn.externals import joblib
+
+def train_model():
+    # TODO: determine feature structures. then train the model.
+    data = pd.read_csv('./samples/GET.csv')
+    print(data.shape)
+
+    # remove unused data column
+    data.drop('uagent', axis=1, inplace=True)
+
+    # data separation
+    X = data.drop('label', axis=1)  
+    y = data['label']
+    X = data.select_dtypes(include=[object])
+
+    # fit and transform
+    label_encoder = LabelEncoder()
+    X_t = X.apply(label_encoder.fit_transform)
+
+    # print(X_t)
+
+    # onehot_encoder = OneHotEncoder(categories='auto',sparse=False)
+    # onehot_encoder.fit(X_t)
+    # onehotlabels = label_encoder.transform(X_t).toarray()
+    # print(onehotlabels.shape)
+
+    X_train, X_test, y_train, y_test = train_test_split(X_t, y, test_size = 0.20)
+
+    svclassifier = SVC(kernel='linear')  
+    svclassifier.fit(X_train, y_train)  
+
+    y_pred = svclassifier.predict(X_test)
+
+    conf = confusion_matrix(y_test,y_pred)
+    rept = classification_report(y_test,y_pred)
+    acc = accuracy_score(y_pred, y_test)
+
+    plt.imshow(conf, cmap='binary', interpolation='None')
+    plt.show()
+
+    model_columns = list(X.columns)
+
+    joblib.dump(svclassifier, './models/model.pkl')
+
+if __name__ == "__main__":
+    train_model()
