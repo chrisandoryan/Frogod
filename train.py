da19c04d6e4d96f5595bc4fe404c629fef21b42a
chrisandoryan <chrisando.siahaan@gmail.com>
2019-06-27 12:32:36 +0700

    improved svm training and bug fix

--- train.py
+++ train.py
@@ -1,7 +1,8 @@
 from sklearn import svm
 import pandas as pd  
 import numpy as np  
-import matplotlib.pyplot as plt  
+import matplotlib.pyplot as plt
+from mlxtend.plotting import plot_decision_regions
 
 from sklearn.model_selection import train_test_split  
 from sklearn.svm import SVC  
@@ -13,23 +14,28 @@ from sklearn.externals import joblib
 
 def train_model():
     # TODO: determine feature structures. then train the model.
-    data = pd.read_csv('./samples/GET.csv')
+    data = pd.read_csv('./samples/GET_new.csv')
     print(data.shape)
+    print(data.head())
 
     # remove unused data column
     data.drop('uagent', axis=1, inplace=True)
+    data.drop('reqmethod', axis=1, inplace=True)
+    data.drop('payload', axis=1, inplace=True)
+    data.drop('class', axis=1, inplace=True)
 
     # data separation
     X = data.drop('label', axis=1)  
+    X = X.select_dtypes(include=[object])
     y = data['label']
-    X = data.select_dtypes(include=[object])
 
     # fit and transform
     label_encoder = LabelEncoder()
     X_t = X.apply(label_encoder.fit_transform)
+    y_t = label_encoder.fit(y).transform(y)
 
-    print(X_t)
-    input()
+    # print(X_t)
+    # input()
 
     # onehot_encoder = OneHotEncoder(categories='auto',sparse=False)
     # onehot_encoder.fit(X_t)
@@ -38,7 +44,7 @@ def train_model():
 
     X_train, X_test, y_train, y_test = train_test_split(X_t, y, test_size = 0.40)
 
-    svclassifier = SVC(kernel='rbc')  
+    svclassifier = SVC(kernel='linear')  
     svclassifier.fit(X_train, y_train)  
 
     y_pred = svclassifier.predict(X_test)
@@ -51,10 +57,22 @@ def train_model():
     print(rept)
     print(acc)
 
-    plt.imshow(conf, cmap='binary', interpolation='None')
-    plt.show()
+    # plot_decision_regions(
+    #     X=X_t.values, 
+    #     y=y_t, clf=svclassifier, 
+    #     legend=2,
+    #     feature_index=[0,1],
+    # )
+
+    # plt.xlabel(X_t.columns[0], size=14)
+    # plt.ylabel(X_t.columns[1], size=14)
+    # plt.title('SVM Decision Region Boundary', size=16)
+    # plt.show()
+
+    # plt.imshow(conf, cmap='binary', interpolation='None')
 
     model_columns = list(X.columns)
+    print(model_columns)
 
     joblib.dump(svclassifier, './models/model.pkl')
 
